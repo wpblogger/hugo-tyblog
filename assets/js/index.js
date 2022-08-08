26ac22a9e7649e80fa1e601b107460114de1c95f
wpblogger <wpblogger@mail.ru>
2022-08-08 19:52:28 +0300

    Add sidebar

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,7 +6,6 @@ function reveal() {
 
   for (var i = 0; i < rev.length; i++) {
     var elementTop = rev[i].getBoundingClientRect().top;
-    console.log('top = ', elementTop);
     if (elementTop < 0) {
       hd[i].classList.add("theme-header-stycky");
     } else {
@@ -16,3 +15,45 @@ function reveal() {
 }
 
 window.addEventListener("scroll", reveal);
+
+window.onload=function(){
+  var popupBtn = document.getElementById("popup-btn");
+  if(popupBtn){
+    popupBtn.addEventListener("click", function(event) {
+      event.preventDefault();
+      var data = httpGet(event.target.getAttribute("href"));
+      addContent(data);
+    });
+  }
+}
+
+function httpGet(theUrl) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open( "GET", theUrl, false );
+  xmlHttp.send( null );
+  var parser = new DOMParser();
+  var doc = parser.parseFromString(xmlHttp.responseText, 'text/html');
+  return doc.querySelector('main').innerHTML;
+}
+
+function addContent(data) {
+  var el = document.getElementById("popupOverlay");
+  el.classList.add("active");
+  el = document.getElementById("popupContent");
+  el.classList.add("active");
+  el = document.getElementById("popupData");
+  el.innerHTML = data;
+  var head = el.querySelector('h1');
+  console.log(head);
+  document.getElementById("popupHead").innerHTML = "<h1>"+head.innerHTML+"</h1><span class=\"close\" onclick=\"removeContent()\"></span>";
+  el = document.getElementById("popupOverlay");
+}
+
+function removeContent() {
+  var el = document.getElementById("popupOverlay");
+  el.classList.remove("active");
+  el = document.getElementById("popupContent");
+  el.classList.remove("active");
+  el = document.getElementById("popupData");
+  el.innerHTML = "";
+}
